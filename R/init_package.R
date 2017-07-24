73104b8dde141cb67cf43b5147bbd5d754bbbfe4
lbusett <lbusett@gmail.com>
2017-07-24 13:54:30 +0200

    package skeleton

--- R/init_package.R
+++ R/init_package.R
@@ -1,17 +1,12 @@
-
-init_package <- function() {
-  require(devtools)
   # devtools::create()
   devtools::use_dev_version()
   devtools::use_data()
   devtools::use_readme_rmd()
-  devtools::use_build_ignore()
   devtools::use_coverage()
   devtools::use_cran_comments()
   devtools::use_gpl3_license()
   devtools::use_rstudio()
-  devtools::uses_testthat()
+  devtools::use_testthat()
   devtools::use_vignette()
   devtools::use_travis()
   devtools::use_news_md()
-}
