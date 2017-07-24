91454a54b52225f02e9cc4b1b9f873410da1f957
lbusett <lbusett@gmail.com>
2017-07-24 19:11:24 +0200

    added init_sinew

--- R/init_sinew.R
+++ R/init_sinew.R
@@ -1,5 +1,7 @@
-#' @title Initialize SINEW opts for roxygenize
+#' @title init_sinew
 #' @description FUNCTION_DESCRIPTION
+#' @param add_fields PARAM_DESCRIPTION, Default: c("details", "examples", "rdname", "export", "author")
+#' @param author PARAM_DESCRIPTION, Default: as.character(person("Lorenzo", "Busetto, phD (2017)", email = "lbusett@gmail.com"))
 #' @return OUTPUT_DESCRIPTION
 #' @details DETAILS
 #' @examples
@@ -11,9 +13,12 @@
 #' @rdname init_sinew
 #' @export
 #' @author Lorenzo Busetto, phD (2017) <lbusett@gmail.com>
-init_sinew <- function() {
-  sinew_opts$set(add_fields = c("details", "examples", "rdname", "export", "author"))
-  sinew_opts$set(author    = as.character(person("Lorenzo", "Busetto, phD (2017)", email = "lbusett@gmail.com")))
-  message("current add_fields: ", sinew_opts$get("add_fields"))
+#' @importFrom sinew sinew_opts
+#'
+init_sinew <- function(add_fields = c("details", "examples", "rdname", "export", "author"),
+                       author = as.character(person("Lorenzo", "Busetto, phD (2017)", email = "lbusett@gmail.com"))) {
+  sinew::sinew_opts$set(add_fields = add_fields)
+  sinew::sinew_opts$set(author     = author)
+  message("current add_fields: ", paste(sinew_opts$get("add_fields"), collapse = " "))
   message("current author: ", sinew_opts$get("author"))
 }
