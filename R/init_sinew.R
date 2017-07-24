bf94fbe5d5a6f2e0ee797e08460145aa050820c1
lbusett <lbusett@gmail.com>
2017-07-24 14:22:17 +0200

    as above -
    remember that non-runnign scripts must bekept under "stubs" !!!

--- R/init_sinew.R
+++ R/init_sinew.R
@@ -1,3 +1,16 @@
+#' @title Initialize SINEW opts for roxygenize
+#' @description FUNCTION_DESCRIPTION
+#' @return OUTPUT_DESCRIPTION
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  #EXAMPLE1
+#'  }
+#' }
+#' @rdname init_sinew
+#' @export
+#' @author Lorenzo Busetto, phD (2017) <lbusett@gmail.com>
 init_sinew <- function() {
   sinew_opts$set(add_fields = c("details", "examples", "rdname", "export", "author"))
   sinew_opts$set(author    = as.character(person("Lorenzo", "Busetto, phD (2017)", email = "lbusett@gmail.com")))
